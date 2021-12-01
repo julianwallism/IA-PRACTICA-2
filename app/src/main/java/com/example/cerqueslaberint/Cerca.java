975ef23434f41b0a014944fec01807f68592cac8
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2021-12-01 16:01:25 +0100

    Comments

--- app/src/main/java/com/example/cerqueslaberint/Cerca.java
+++ app/src/main/java/com/example/cerqueslaberint/Cerca.java
@@ -1,13 +1,10 @@
 package com.example.cerqueslaberint;
 
-import java.sql.SQLOutput;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * Created by Ramon Mas on 10/10/21.
@@ -15,7 +12,7 @@ import java.util.Stack;
  */
 
 /**
- * AUTORS:__________________________________________
+ * AUTORS: Jonathan Salisbury, Julián Wallis, Juan Ignacio Velàzquez
  */
 /* S'ha d'omplenar la següent taula amb els diferents valors del nodes visitats i la llargada del camí
  * per les diferents grandàries de laberints proposades i comentar breument els resultats obtinguts.
@@ -28,7 +25,8 @@ import java.util.Stack;
  *    Gran
  *
  * Comentari sobre els resultats obtinguts:
- *
+ * En general ens ha semblat que la cerca en profunditat rendeix pitjor que la cerca en amplada
+ * en la majoria dels casos.
  *
  *
  *
@@ -37,22 +35,19 @@ import java.util.Stack;
  */
 
 public class Cerca {
+
     static final public int MANHATTAN = 2;
     static final public int EUCLIDEA = 3;
 
     Laberint laberint;      // laberint on es cerca
     int files, columnes;    // files i columnes del laberint
 
+    /* Constants de moviment */
     static final int[] DIRECCIONS = {Laberint.AMUNT, Laberint.DRETA, Laberint.AVALL, Laberint.ESQUERRA};
-    static final int[] OFFSET_X = {-1, 0, 1, 0}; //
+    static final int[] OFFSET_X = {-1, 0, 1, 0};
     static final int[] OFFSET_Y = {0, 1, 0, -1};
-    static final int[][] PERMUTACIONES = {{1, 2, 3, 4}, {1, 2, 4, 3}, {1, 3, 2, 4}, {1, 3, 4, 2}, {1, 4, 2, 3},
-            {1, 4, 3, 2}, {2, 1, 3, 4}, {2, 1, 4, 3}, {2, 3, 1, 4}, {2, 3, 4, 1},
-            {2, 4, 1, 3}, {2, 4, 3, 1}, {3, 1, 2, 4}, {3, 1, 4, 2}, {3, 2, 1, 4},
-            {3, 2, 4, 1}, {3, 4, 1, 2}, {3, 4, 2, 1}, {4, 1, 2, 3}, {4, 1, 3, 2},
-            {4, 2, 1, 3}, {4, 2, 3, 1}, {4, 3, 1, 2}, {4, 3, 2, 1}};
 
-
+    /* Variables */
     boolean[][] visitats;
 
     public Cerca(Laberint l) {
@@ -62,6 +57,13 @@ public class Cerca {
         visitats = new boolean[files][columnes];
     }
 
+    /**
+     * Mètode que resól la cerca del laberint mitjançant recorrgut en amplada.
+     *
+     * @param origen Punt inicial del qual es parteix.
+     * @param desti  Punt final al que s'ha de arribar.
+     * @return el cami solució generat.
+     */
     public Cami CercaEnAmplada(Punt origen, Punt desti) {
         Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
@@ -77,7 +79,7 @@ public class Cerca {
                 break;
             } else {
                 ArrayList<Punt> successors = generaSuccessors(punt);
-                visita(punt);
+                visitats[punt.x][punt.y] = true;
                 for (Punt successor : successors) {
                     oberts.afegeix(successor);
                 }
@@ -87,6 +89,13 @@ public class Cerca {
         return camiTrobat;
     }
 
+    /**
+     * Mètode que resól la cerca del laberint mitjançant recorrgut en profunditat.
+     *
+     * @param origen Punt inicial del qual es parteix.
+     * @param desti  Punt final al que s'ha de arribar.
+     * @return el cami solució generat.
+     */
     public Cami CercaEnProfunditat(Punt origen, Punt desti) {
         Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
@@ -102,7 +111,7 @@ public class Cerca {
                 break;
             } else {
                 ArrayList<Punt> successors = generaSuccessors(punt);
-                visita(punt);
+                visitats[punt.x][punt.y] = true;
                 for (Punt successor : successors) {
                     oberts.push(successor);
                 }
@@ -112,6 +121,14 @@ public class Cerca {
         return camiTrobat;
     }
 
+    /**
+     * Mètode que resól la cerca del laberint mitjançant recorrgut en amplada.
+     *
+     * @param origen Punt inicial del qual es parteix.
+     * @param desti  Punt final al que s'ha de arribar.
+     * @param tipus  Enter que representa el tipus d'heuristica a utilitzar.
+     * @return el cami solució generat.
+     */
     public Cami CercaAmbHeurística(Punt origen, Punt desti, int tipus) {   // Tipus pot ser MANHATTAN o EUCLIDIA
         int i;
         Cami camiTrobat = new Cami(files * columnes);
@@ -143,103 +160,60 @@ public class Cerca {
         return camiTrobat;
     }
 
-    private double heuristica(Punt punt, Punt desti, int tipus) {
-        if (tipus == EUCLIDEA)
-            return euclidea(punt, desti);
-        else
-            return manhattan(punt, desti);
-    }
-
-    private double euclidea(Punt punt, Punt desti) {
-        float x = Math.abs(punt.x - desti.x);
-        float y = Math.abs(punt.y - desti.y);
-        return Math.sqrt(x * x + y * y);
-    }
-
-    private double manhattan(Punt punt, Punt desti) {
-        float x = Math.abs(punt.x - desti.x);
-        float y = Math.abs(punt.y - desti.y);
-        return x + y;
-    }
-
-    public Cami CercaViatjant(Punt origen, Punt desti) {
-/*        laberint.setNodes(0);
+    /**
+     * Mètode que obté el camí més curt entre l'origen i el destí passant per els cuatre nodes.
+     *
+     * @param origen Punt inicial del qual es parteix.
+     * @param desti  Punt final al que s'ha de arribar.
+     * @return el cami solució generat.
+     */
+    Cami CercaViatjant(Punt origen, Punt desti) {
+        laberint.setNodes(0);
         // Implementa l'algoritme aquí
-        //Calculamos distancias entre punto inicial y los puntos a visitar
-        int[][] dist = new int[6][6];
-        for (int i = 0; i < 4; i++) {
-            dist[0][i + 1] = CercaAmbHeurística(origen, laberint.getObjecte(i), EUCLIDEA).longitud;
-        }
-        //Calculamos distancias entre los 4 puntos a visitar
-        for (int i = 0; i < 3; i++) {
-            for (int j = i + 1; j < 4; j++) {
-                dist[i + 1][j + 1] = CercaAmbHeurística(laberint.getObjecte(i), laberint.getObjecte(j), EUCLIDEA).longitud;
-                dist[j + 1][i + 1] = CercaAmbHeurística(laberint.getObjecte(i), laberint.getObjecte(j), EUCLIDEA).longitud;
-            }
-        }
-        //Calculamos distancias de los 4 puntos a visitar al destino
-        for (int i = 0; i < 4; i++) {
-            dist[i + 1][5] = CercaAmbHeurística(laberint.getObjecte(i), desti, EUCLIDEA).longitud;
-        }
-        //ATENTOS AL CODIGO ESPAGUETI:
-        int[] permutacion = new int[4];
-        int coste = 9999;
-        for (int[] perm : PERMUTACIONES) {
-            //coste principio -> primer nodo
-            int costeAux = dist[0][perm[0]];
-            //coste entre nodos
-            for (int i = 0; i < 3; i++) {
-                costeAux += dist[perm[i]][perm[i + 1]];
-            }
-            //Coste ultimo nodo -> destino
-            costeAux += dist[perm[3]][5];
-            if (costeAux < coste) {
-                coste = costeAux;
-                permutacion = perm;
-            }
-        }
-        Cami camiTrobat = new Cami(coste);
-        System.out.println("permutacion: " + java.util.Arrays.toString(permutacion) + " Coste: " + coste);
-        for (int[] row : dist) {
-            System.out.println(java.util.Arrays.toString(row));
-        }
-        //camiAux= CercaAmbHeurística(laberint.getObjecte(permutacion[3]),desti,EUCLIDEA);
-        Cami camiAux = CercaEnAmplada(laberint.getObjecte(permutacion[3] - 1), desti);
-        for (int i = 0; i < camiAux.longitud - 1; i++) {
-            camiTrobat.afegeix(camiAux.cami[i]);
-        }
-        for (int i = 3; i > 0; i--) {
-            // camiAux=CercaAmbHeurística(laberint.getObjecte(permutacion[i]-1),laberint.getObjecte(permutacion[i+1]-1),EUCLIDEA);
-            camiAux = CercaEnAmplada(laberint.getObjecte(permutacion[i] - 1), laberint.getObjecte(permutacion[i - 1] - 1));
-            for (int j = camiAux.longitud - 1; j > 0; j--) {
-                camiTrobat.afegeix(camiAux.cami[j]);
-            }
-        }
-
-        //Cami camiAux= CercaAmbHeurística(origen,laberint.getObjecte(permutacion[0]-1),EUCLIDEA);
-        camiAux = CercaEnAmplada(origen, laberint.getObjecte(permutacion[0] - 1));
-        for (int i = 0; i < camiAux.longitud - 1; i++) {
-            camiTrobat.afegeix(camiAux.cami[i]);
-        }
-        return camiTrobat;*/
-        return viatjant(origen, desti);
-    }
-
-    private Cami viatjant(Punt origen, Punt desti){
         Integer[] nodes = {0, 1, 2, 3};
         List<Integer[]> camins = permutacions(nodes);
         Cami camiFinal = null;
         camiFinal.longitud = Integer.MAX_VALUE;
-        for(Integer[] cami: camins){
+        for (Integer[] cami : camins) {
             Cami candidat = generaCami(cami, origen, desti);
-            if(candidat.longitud < camiFinal.longitud){
+            if (candidat.longitud < camiFinal.longitud) {
                 camiFinal = candidat;
             }
         }
         return camiFinal;
     }
 
-    private Cami generaCami(Integer[] cami, Punt origen, Punt desti){
+    /**
+     * Mètode que calcula la distancia entre dos punts fent servir la heuristica indicada.
+     *
+     * @param punt  Punt inicial per calcular la distancia
+     * @param desti Punt final per calcular la distancia
+     * @param tipus Enter que representa el tipus d'heuristica a utilitzar.
+     * @return el modul de la distancia amb la heuristica aplicada.
+     */
+    private double heuristica(Punt punt, Punt desti, int tipus) {
+        float x, y;
+        if (tipus == EUCLIDEA) {
+            x = Math.abs(punt.x - desti.x);
+            y = Math.abs(punt.y - desti.y);
+            return Math.sqrt(x * x + y * y);
+        } else {
+            x = Math.abs(punt.x - desti.x);
+            y = Math.abs(punt.y - desti.y);
+            return x + y;
+        }
+    }
+
+    /**
+     * Mètode que genera el camí entre l'origen i el desti passant per tots els nodes en l'ordre
+     * indicat dins l'array cami.
+     *
+     * @param cami   Ordre pel que hem de passar pels nodes.
+     * @param origen Punt d'origen del cami a cercar
+     * @param desti  Punt desti del cami a cercar
+     * @return el cami solució generat.
+     */
+    private Cami generaCami(Integer[] cami, Punt origen, Punt desti) {
         Cami candidat = new Cami(files * columnes * cami.length);
         Punt darrerNode = laberint.getObjecte(cami[cami.length - 1]);
         Cami darrerNodeDesti = CercaEnAmplada(darrerNode, desti);
@@ -256,8 +230,15 @@ public class Cerca {
         return candidat;
     }
 
-    private Cami concatenaCami(Cami origen, Cami destinatari){
-        for(Punt p: origen.cami) {
+    /**
+     * Mètode que concatena el cami origen al final del cami destinatari
+     *
+     * @param origen      cami d'on s'obtenen les dades
+     * @param destinatari cami on es concatenen les dades
+     * @return nou cami concatenat
+     */
+    private Cami concatenaCami(Cami origen, Cami destinatari) {
+        for (Punt p : origen.cami) {
             if (p != null) {
                 destinatari.afegeix(p);
             }
@@ -265,30 +246,60 @@ public class Cerca {
         return destinatari;
     }
 
-    private static <T> List<T[]> permutacions(T[] original){
+    /**
+     * Mètode 'wrapper' per a la funció recursiva de permutacions, aquest mètode crea la llista
+     * resultant, crida al mètode recursiu i retorna la llista generada.
+     *
+     * @param original Array amb els elements originals a permutar
+     * @param <T>      Generic per a que el metode funcioni per a tots els tipus de dades.
+     * @return Llista amb totes les permutacions de l'array original
+     */
+    private static <T> List<T[]> permutacions(T[] original) {
         List<T[]> permutacions = new ArrayList<>();
-        permutacionsHelper(original, permutacions, original.length);
+        permutacionsRecursiu(original, permutacions, original.length);
         return permutacions;
     }
 
-    private static <T> void permutacionsHelper(T[] permutacio, List<T[]> permutacions, int n) {
+    /**
+     * Mètode recursiu que genera totes les permutaciones dels continguts d'un array.
+     *
+     * @param permutacio   permutació actual
+     * @param permutacions Llista on es guarden totes les permutacions generades
+     * @param n            longitud de la permutació actual
+     * @param <T>          Generic per a que el metode funcioni per a tots els tipus de dades.
+     */
+    private static <T> void permutacionsRecursiu(T[] permutacio, List<T[]> permutacions, int n) {
         if (n <= 1) {
             permutacions.add(permutacio);
         }
         T[] permutacioTemp = Arrays.copyOf(permutacio, permutacio.length);
         for (int i = 0; i < n; i++) {
             intercanvia(permutacioTemp, i, n - 1);
-            permutacionsHelper(permutacioTemp, permutacions, n - 1);
+            permutacionsRecursiu(permutacioTemp, permutacions, n - 1);
             intercanvia(permutacioTemp, i, n - 1); // backtracking
         }
     }
 
+    /**
+     * Mètode que intercanvia dos elements d'un array
+     *
+     * @param array  array que conté els valors a intercanviar.
+     * @param primer primer element a intercanviar
+     * @param segon  segon element a intercanviar
+     * @param <T>    Generic per a que el metode funcioni per a tots els tipus de dades.
+     */
     private static <T> void intercanvia(T[] array, int primer, int segon) {
         T temp = array[primer];
         array[primer] = array[segon];
         array[segon] = temp;
     }
 
+    /**
+     * Mètode que genera tots els punts que es poden visitar desde el punt dondat.
+     *
+     * @param punt punt original
+     * @return Llista dels punts als que es poden anar.
+     */
     private ArrayList<Punt> generaSuccessors(Punt punt) {
         ArrayList<Punt> successors = new ArrayList<>();
         Punt successor;
@@ -302,14 +313,23 @@ public class Cerca {
         return successors;
     }
 
-    private void visita(Punt p) {
-        visitats[p.x][p.y] = true;
-    }
-
+    /**
+     * Mètode que comprova si el punt donat esta dins el laberint.
+     *
+     * @param p punt a comprovar
+     * @return boolean que representa si el punt es dintre o no.
+     */
     private boolean esPosicioCorrecta(Punt p) {
         return p.x >= 0 && p.x < columnes && p.y >= 0 && p.y < files && !visitats[p.x][p.y];
     }
 
+    /**
+     * Mètode que genera el cami entre dos punts mitjançant els punts previs.
+     *
+     * @param inici Punt inicial del camí
+     * @param fi    Punt final del camí
+     * @param cami  Cami generat entre eles dos punts
+     */
     private void generaCami(Punt inici, Punt fi, Cami cami) {
         while (inici != null && !inici.equals(fi)) {
             cami.afegeix(inici);
